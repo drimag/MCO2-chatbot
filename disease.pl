273b6bffcb0c4aaa4f07224852fa53bb91adc20e
drimag <106684744+drimag@users.noreply.github.com>
2023-04-06 03:55:32 +0800

    Add files via upload

--- disease.pl
+++ disease.pl
@@ -1,4 +1,4 @@
-% declaring the diseases and symptoms 
+% declaring the diseases and symptoms
 diseases([dengue, pneumonia, acs, tuberculosis, uti, hypertension, scabies, asthma]).
 
 disease(Disease) :-
@@ -10,7 +10,7 @@ symptoms_pneumonia([cough, fever, m_j_pain, nausea_vomit, runny_nose, crackels])
 symptoms_acs([heaviness, chest_pain, doom, pain_l_s, acs_sweat]).
 symptoms_tuberculosis([cough, cough_blood, night_sweats, fever, chest_pain, fatigue, long_time]).
 symptoms_uti([pain_urine, f_u_urine, inc_voiding, blood_urine, pain_uti, fever_chills]).
-symptoms_hypertension([long_bp]).
+symptoms_hypertension([long_bp, nape_pain, easily_tired]).
 symptoms_scabies([rash_scabies, itches, burrows, sores, crusts]).
 symptoms_asthma([hard_breathing, shortness_breath, wheezing]).
 
@@ -42,15 +42,60 @@ symptom_of(asthma, Symptom) :-
 symptom(Symptom) :-
     setof(Symp, Disease^(disease(Disease), symptom_of(Disease, Symp)), Symptoms),
     member(Symptom, Symptoms).
-% 
-% end of declaring diseases and symptoms 
-% 
+%
+% end of declaring diseases and symptoms
+%
+
+% helper, determines if a certain number of conditions is true or not.
+
+at_least(0, _) :- !.
+at_least(N, [H|T]) :-
+    (call(H), N1 is N-1; N1 = N),
+    at_least(N1, T).
+
+% definitions for when a disease is likely
+have(flu_like) :-
+    at_least(3, [have_symptom(cough), have_symptom(fever), have_symptom(m_j_pain), have_symptom(nausea_vomit), have_symptom(runny_nose)]).
+
+
+% likely(dengue) :-
+%    have(flu_like),
+
+likely(acs) :-
+    have_symptom(chest_pain),
+    have_symptom(heaviness),
+    (risky(acs); risk(old)).
+
+% likely(tuberculosis) :-
+%    have_symptom(long_time),
+
+likely(uti) :-
+    have_symptom(pain_urine);
+    (at_least(3, [have_symptom(f_u_urine), have_symptom(inc_voiding), have_symptom(blood_urine), have_symptom(pain_uti), have_symptom(fever_chills)])).
+
+% likely(scabies) :-
+%
+
+% likely(asthma) :-
+
+likely(pneumonia) :-
+    have(flu_like),
+    have_symptom(crackels).
+
+
+
+
 
 % patient symptoms added to this list
 :- dynamic patient_symptoms/1.
 patient_symptoms([]).
 
-% hasnt really been used, but says that if a symptom is in patient symptoms then the patient has a symptom
+:- dynamic not_symptoms/1.
+not_symptoms([]).
+
+% but says that if a symptom is in patient symptoms then the patient has
+% a given symptom
+%
 have_symptom(Symptom) :-
     patient_symptoms(Symptoms),
     symptom(Symptom),
@@ -73,13 +118,14 @@ common_symptoms(Symptoms) :-
     findall(DiseaseSymptoms, (member(Disease, Diseases), findall(Symptom, symptom_of(Disease, Symptom), DiseaseSymptoms)), AllSymptoms),
     intersect_lists(AllSymptoms,Symptoms).
 
-% based on possible diseases, returns the possible symptoms 
+% based on possible diseases, returns the possible symptoms, excludes
+% cant be symptoms
 all_possible_symptoms(Symptoms) :-
     possible_diseases(Diseases),
-    findall(DiseaseSymptoms, (member(Disease, Diseases), findall(Symptom, symptom_of(Disease, Symptom), DiseaseSymptoms)), AllSymptoms),
+    not_symptoms(NotSymptoms),
+    findall(DiseaseSymptoms, (member(Disease, Diseases), findall(Symptom, (symptom_of(Disease, Symptom), not(member(Symptom, NotSymptoms))), DiseaseSymptoms)), AllSymptoms),
     flatten(AllSymptoms, Symptoms1),
-    list_to_set(Symptoms1, Symptoms),
-    write(Symptoms).
+    list_to_set(Symptoms1, Symptoms).
 
 % intersection of lists
 intersect_lists([], []).
@@ -99,24 +145,32 @@ add_patient_symptom(Symptom) :-
     retract(patient_symptoms(Symptoms)),
     assertz(patient_symptoms(NewSymptoms)).
 
-% condition for ending the interview, may add more conditions 
-interview_over() :-
-    disease(Disease),
+add_not_symptom(Symptom) :-
+    not_symptoms(NotSymptoms),
+    append(NotSymptoms, [Symptom], NewSymptoms),
+    retract(not_symptoms(NotSymptoms)),
+    assertz(not_symptoms(NewSymptoms)).
+
+% condition for ending the interview, may add more conditions
+interview_not_over() :-
     aggregate_all(count, possible(Disease), Count),
-    Count == 1, !.
+    write(Count),
+    Count > 1, !.
 
 % asking the user for a particular Symptom
 ask(Symptom) :-
     format('do you have ~w? (y/n)~n', [Symptom]),
     read(Answer),
-    Answer == y -> add_patient_symptom(Symptom).
+    (   Answer == y -> add_patient_symptom(Symptom) ;
+    Answer == n -> add_not_symptom(Symptom) ; true).
 
-% the whole loop for the whole thing 
+% the whole loop for the whole thing
 interview() :-
-    interview_over(),
-    possible_symptoms(PossibleSymptoms),
+    interview_not_over(),
+    all_possible_symptoms(PossibleSymptoms),
     common_symptoms(CommonSymptoms),
     subtract(PossibleSymptoms, CommonSymptoms, SymptomsOI),
+    write(SymptomsOI),
     nth0(0, SymptomsOI, SymptomOI),
     ask(SymptomOI),
     interview().

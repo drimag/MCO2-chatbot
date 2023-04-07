35d80a5fe1360542ca5fc484b49c38a6b860ee71
drimag <106684744+drimag@users.noreply.github.com>
2023-04-07 22:04:40 +0800

    Add files via upload

--- disease.pl
+++ disease.pl
@@ -6,13 +6,28 @@ disease(Disease) :-
     member(Disease, Diseases).
 
 symptoms_dengue([cough, fever, m_j_pain, nausea_vomit, runny_nose, dengue_rash]).
-symptoms_pneumonia([cough, fever, m_j_pain, nausea_vomit, runny_nose, crackels]).
-symptoms_acs([heaviness, chest_pain, doom, pain_l_s, acs_sweat]).
-symptoms_tuberculosis([cough, cough_blood, night_sweats, fever, chest_pain, fatigue, long_time]).
+symptoms_pneumonia([cough, fever, m_j_pain, nausea_vomit, runny_nose, crackels, shortness_breath]).
+symptoms_acs([heaviness, chest_pain, doom, pain_l_s, acs_sweat, shortness_breath]).
+symptoms_tuberculosis([cough, cough_blood, night_sweats, fever, chest_pain, fatigue]).
 symptoms_uti([pain_urine, f_u_urine, inc_voiding, blood_urine, pain_uti, fever_chills]).
-symptoms_hypertension([long_bp, nape_pain, easily_tired]).
+symptoms_hypertension([headache, dizziness, shortness_breath, high_bp]).
 symptoms_scabies([rash_scabies, itches, burrows, sores, crusts]).
-symptoms_asthma([hard_breathing, shortness_breath, wheezing]).
+symptoms_asthma([shortness_breath, wheezing, persistent_cough, chest_tight]).
+symptoms_acgas([diarrhea, fever, cramps, dehydration, nausea_vomit]).
+symptoms_heatstroke([high_body_temp, headache, dizziness, nausea_vomit, disorientation]).
+
+mins_dengue([fever, m_j_pain, dengue_rash]).
+mins_acs([chest_pain, pain_l_s]).
+mins_pneumonia([cough, fever, crackels]).
+mins_tuberculosis([cough, fever, night_sweats]).
+mins_uti([pain_urine, f_u_urine]).
+mins_hypertension([high_bp]).
+mins_scabies([itches, rash_scabies]).
+mins_asthma([wheezing, shortness_breath]).
+mins_acgas([diarrhea]).
+mins_heatstroke([high_body_temp, disorientation]).
+
+
 
 symptom_of(dengue, Symptom) :-
     symptoms_dengue(Symptoms),
@@ -38,49 +53,91 @@ symptom_of(scabies, Symptom) :-
 symptom_of(asthma, Symptom) :-
     symptoms_asthma(Symptoms),
     member(Symptom,Symptoms).
+symptom_of(acgas, Symptom) :-
+    symptoms_acgas(Symptoms),
+    member(Symptom,Symptoms).
+symptom_of(heatstroke, Symptom) :-
+    symptoms_heatstroke(Symptoms),
+    member(Symptom,Symptoms).
+
+min_symptom_of(dengue, Symptom) :-
+    mins_dengue(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(pneumonia, Symptom) :-
+    mins_pneumonia(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(acs, Symptom) :-
+    mins_acs(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(tuberculosis, Symptom) :-
+    mins_tuberculosis(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(uti, Symptom) :-
+    mins_uti(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(hypertension, Symptom) :-
+    mins_hypertension(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(scabies, Symptom) :-
+    mins_scabies(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(asthma, Symptom) :-
+    mins_asthma(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(acgas, Symptom) :-
+    mins_acgas(Symptoms),
+    member(Symptom,Symptoms).
+min_symptom_of(heatstroke, Symptom) :-
+    mins_heatstroke(Symptoms),
+    member(Symptom,Symptoms).
 
 symptom(Symptom) :-
     setof(Symp, Disease^(disease(Disease), symptom_of(Disease, Symp)), Symptoms),
     member(Symptom, Symptoms).
+
+
+
 %
 % end of declaring diseases and symptoms
 %
 
-% helper, determines if a certain number of conditions is true or not.
-
-at_least(0, _) :- !.
-at_least(N, [H|T]) :-
-    (call(H), N1 is N-1; N1 = N),
-    at_least(N1, T).
-
 % definitions for when a disease is likely
-have(flu_like) :-
-    at_least(3, [have_symptom(cough), have_symptom(fever), have_symptom(m_j_pain), have_symptom(nausea_vomit), have_symptom(runny_nose)]).
 
-
-% likely(dengue) :-
-%    have(flu_like),
+hi_sus(dengue) :-
+    mins_possible(dengue),
+    (   trisk(turniquet); trisk(close_contact)).
 
 hi_sus(acs) :-
     have_symptom(chest_pain),
     have_symptom(heaviness),
     (risky(acs); risk(old)).
 
-% likely(tuberculosis) :-
-%    have_symptom(long_time),
+hi_sus(tuberculosis) :-
+   mins_possible(tuberculosis),
+   trisk(long_time).
 
 hi_sus(uti) :-
-    have_symptom(pain_urine);
-    (at_least(3, [have_symptom(f_u_urine), have_symptom(inc_voiding), have_symptom(blood_urine), have_symptom(pain_uti), have_symptom(fever_chills)])).
+    mins_possible(uti).
 
-% likely(scabies) :-
-%
+hi_sus(scabies) :-
+    mins_possible(scabies),
+    trisk(close_contact).
 
-% likely(asthma) :-
+hi_sus(asthma) :-
+    mins_possible(asthma).
 
 hi_sus(pneumonia) :-
-    have(flu_like),
-    have_symptom(crackels).
+    mins_possible(pneumonia).
+
+hi_sus(hypertension) :-
+    mins_possible(hypertension),
+    trisk(monitor_bp).
+
+hi_sus(heatstroke) :-
+    mins_possible(heatstroke).
+
+hi_sus(acgas) :-
+    mins_possible(acgas).
 
 
 
@@ -108,13 +165,20 @@ possible(Disease) :-
     patient_symptoms(PatientSymptoms),
     subset(PatientSymptoms,Symptoms).
 
+mins_possible(Disease) :-
+    disease(Disease),
+    setof(Symptom, min_symptom_of(Disease, Symptom), MinSymptoms),
+    patient_symptoms(PatientSymptoms),
+    subset(MinSymptoms,PatientSymptoms).
+
 % just puts the possible diseases into a list
 possible_diseases(Diseases) :-
     setof(Disease, possible(Disease), Diseases).
 
-
-likely_diseases(Diseases) :-
-    setof(Disease, likely(Disease), Diseases).
+% list of diseases where the patient has that minimum symptoms for it to
+% be considered
+mins_diseases(Diseases) :-
+    setof(Disease, mins_possible(Disease), Diseases).
 
 
 % based on possible diseases, returns the common symptoms
@@ -158,6 +222,7 @@ add_not_symptom(Symptom) :-
 
 % condition for ending the symptoms interview, may add more conditions
 si_not_over() :-
+    disease(Disease),
     aggregate_all(count, possible(Disease), Count),
     write(Count),
     Count > 1, !.
@@ -190,6 +255,9 @@ disease_questions(Disease, Questions) :-
 
     ).
 
+
+
+
 % loop for asking for symptoms
 symptoms_interview() :-
     si_not_over(),
@@ -219,8 +287,8 @@ suggest(Diseases) :-
 interview() :-
     symptoms_interview(),
     trisks_interview(),
-    likely_diseases(LikelyDiseases),
+    possible_diseases(PossibleDiseases),
     (
         ( possible(Disease), hi_sus(Disease) ) ->
-        diagnose(Disease); suggest(LikelyDiseases)
+        diagnose(Disease); suggest(PossibleDiseases)
     ).

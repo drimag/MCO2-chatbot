7a43792312099bd5a03c2c16126e2f343beeaa9e
drimag <106684744+drimag@users.noreply.github.com>
2023-04-10 12:39:32 +0800

    Add files via upload

--- disease.pl
+++ disease.pl
@@ -1,3 +1,5 @@
+:- use_module(library(random)).
+
 % declaring the diseases and symptoms
 diseases([dengue, pneumonia, acs, tuberculosis, uti, hypertension, scabies, asthma]).
 
@@ -7,7 +9,7 @@ disease(Disease) :-
 
 symptoms_dengue([cough, fever, m_j_pain, nausea_vomit, runny_nose, dengue_rash]).
 symptoms_pneumonia([cough, fever, m_j_pain, nausea_vomit, runny_nose, crackels, shortness_breath]).
-symptoms_acs([heaviness, chest_pain, doom, pain_l_s, acs_sweat, shortness_breath]).
+symptoms_acs([heaviness, chest_pain, doom, pain_l_s, acs_sweat, shortness_breath, headache, dizziness, high_bp]).
 symptoms_tuberculosis([cough, cough_blood, night_sweats, fever, chest_pain, fatigue]).
 symptoms_uti([pain_urine, f_u_urine, inc_voiding, blood_urine, pain_uti, fever_chills]).
 symptoms_hypertension([headache, dizziness, shortness_breath, high_bp]).
@@ -97,11 +99,15 @@ symptom(Symptom) :-
 symptoms(Symptoms) :-
     setof(Symptom, symptom(Symptom), Symptoms).
 
+% trisks_dengue([turniquet, close_contact]).
+% trisks_acs([old, smoking, family_history, obesity, high_bp,
+% high_cholesterol]). trisks_tuberculosis([long_time]).
+% trisks_hypertension([monitor_bp]). trisks_scabies([close_contact]).
 
-
-
-
-
+% acs_trisk(X) :- trisks_acs(ACS_Trisks), member(X, ACS_Trisks).
+% trisks_acs([smoking, family_history, obesity, high_bp,
+% high_cholesterol]).
+% [smoking, family_history, obesity, high_bp, high_cholesterol]
 %
 % end of declaring diseases and symptoms
 %
@@ -110,23 +116,34 @@ symptoms(Symptoms) :-
 
 hi_sus(dengue) :-
     mins_possible(dengue),
-    (trisk(turniquet); trisk(close_contact)).
+    (have_trisk(turniquet); have_trisk(close_contact)).
 
 hi_sus(acs) :-
     have_symptom(chest_pain),
     have_symptom(heaviness),
-    (risky(acs); risk(old)).
+    (
+        at_least( 3,
+                [
+                 have_trisk(smoking),
+                 have_trisk(obesity),
+                 have_trisk(high_bp),
+                 have_trisk(high_cholesterol),
+                 have_trisk(family_history)
+                ]
+                );
+        have_trisk(old)
+    ).
 
 hi_sus(tuberculosis) :-
-   mins_possible(tuberculosis),
-   trisk(long_time).
+    mins_possible(tuberculosis),
+    have_trisk(long_time).
 
 hi_sus(uti) :-
     mins_possible(uti).
 
 hi_sus(scabies) :-
     mins_possible(scabies),
-    trisk(close_contact).
+    have_trisk(close_contact).
 
 hi_sus(asthma) :-
     mins_possible(asthma).
@@ -136,7 +153,7 @@ hi_sus(pneumonia) :-
 
 hi_sus(hypertension) :-
     mins_possible(hypertension),
-    trisk(monitor_bp).
+    have_trisk(monitor_bp).
 
 hi_sus(heatstroke) :-
     mins_possible(heatstroke).
@@ -155,6 +172,10 @@ patient_symptoms([]).
 :- dynamic not_symptoms/1.
 not_symptoms([]).
 
+:- dynamic patient_trisks/1.
+patient_trisks([]).
+
+
 % but says that if a symptom is in patient symptoms then the patient has
 % a given symptom
 %
@@ -162,6 +183,10 @@ have_symptom(Symptom) :-
     patient_symptoms(Symptoms),
     symptom(Symptom),
     member(Symptom,Symptoms).
+
+have_trisk(Trisk) :-
+    patient_trisks(Trisks),
+    member(Trisk, Trisks).
 
 not_have_symptom(Symptom) :-
     not_symptoms(NotSymptoms),
@@ -211,11 +236,6 @@ intersect_lists([], []).
 intersect_lists([L|Ls], Intersection) :-
     foldl(intersection, Ls, L, Intersection).
 
-% not really used
-diagnose(Disease) :-
-    possible(Disease),
-    \+ ((possible(OtherDisease), OtherDisease \= Disease)), !.
-
 
 % adds a symptom to the list patient_symptoms
 add_patient_symptom(Symptom) :-
@@ -223,6 +243,12 @@ add_patient_symptom(Symptom) :-
     append(Symptoms, [Symptom], NewSymptoms),
     retract(patient_symptoms(Symptoms)),
     assertz(patient_symptoms(NewSymptoms)).
+
+add_patient_trisk(Trisk) :-
+    patient_trisks(PatientTrisks),
+    append(PatientTrisks, [Trisk], NewTrisks),
+    retract(patient_trisks(PatientTrisks)),
+    assertz(patient_trisks(NewTrisks)).
 
 add_not_symptom(Symptom) :-
     not_symptoms(NotSymptoms),
@@ -232,24 +258,13 @@ add_not_symptom(Symptom) :-
 
 count_unasked_symptoms(AllSymptoms, PatientSymptoms, NotSymptoms, Count) :-
     append(PatientSymptoms, NotSymptoms, AskedSymptoms),
-    subtract(AllSymptoms, AskedSymptoms, UnaskedSymptoms),
+    findall(Symptom, (member(Symptom, AllSymptoms), \+ member(Symptom, AskedSymptoms)), UnaskedSymptoms),
     length(UnaskedSymptoms, Count).
 
-% condition for ending the symptoms interview, may add more conditions
-pi_not_over() :-
-    possible_diseases(PossibleDiseases),
-    symptoms(AllSymptoms),
-    patient_symptoms(PatientSymptoms),
-    not_symptoms(NotSymptoms),
-    (
-        length(PossibleDiseases, Count),
-        Count > 1
-    ) ;
-    (
-        count_unasked_symptoms(AllSymptoms, PatientSymptoms, NotSymptoms, CountUA),
-        CountUA > 0
-    ).
-
+at_least(0, _) :- !.
+at_least(N, [H|T]) :-
+    (call(H), N1 is N-1; N1 = N),
+    at_least(N1, T).
 
 % asking the user for a particular Symptom
 ask(Symptom) :-
@@ -258,35 +273,99 @@ ask(Symptom) :-
     (   Answer == y -> add_patient_symptom(Symptom) ;
     Answer == n -> add_not_symptom(Symptom) ; true).
 
+ask_trisk(Trisk) :-
+    format('do you have ~w? (y/n)~n', [Trisk]),
+    read(Answer),
+    (Answer == y -> add_patient_trisk(Trisk) ; true).
+
+ask_trisks_acs([]).
+ask_trisks_acs([H|T]) :-
+    \+ at_least(3, [have_trisk(smoking), have_trisk(obesity), have_trisk(high_bp), have_trisk(high_cholesterol), have_trisk(family_history)]),
+    ask_trisk(H),
+    ask_trisks_acs(T).
+
+
+ask_trisks(Disease) :-
+    Disease == dengue -> (
+                   ask_trisk(turniquet),
+                   (
+                       have_trisk(turniquet) -> true ;
+                       ask_trisk(close_contact)
+                   )
+               );
+    Disease == tuberculosis -> ask_trisk(long_time);
+    Disease == hypertension -> ask_trisk(monitor_bp);
+    Disease == scabies -> ask_trisk(close_contact);
+    Disease == acs -> (
+                   ask_trisk(old),
+                   (
+                       have_trisk(old) -> true ;
+
+                       ask_trisks_acs([smoking,
+                                       family_history,
+                                       obesity,
+                                       high_bp,
+                                       high_cholesterol]
+                                     )
+                   )
+               ).
+
 ask_list([]).
 ask_list([H|T]) :-
     ask(H),
     ask_list(T).
 
-scan(Disease) :-
+scan_mins(Disease) :-
      possible(Disease),
      setof(Symptom, min_symptom_of(Disease, Symptom), MinSymptoms),
      patient_symptoms(PatientSymptoms),
      subtract(MinSymptoms, PatientSymptoms, AskSymptoms),
      ask_list(AskSymptoms).
 
+scan_trisks(Disease) :-
+    mins_possible(Disease),
+    ask_trisks(Disease).
+
+% condition for ending the symptoms interview, may add more conditions
+pi_not_over() :-
+    (
+        possible_diseases(PossibleDiseases),
+        length(PossibleDiseases, Count),
+        Count > 1
+    ),
+    (
+        symptoms(AllSymptoms),
+        patient_symptoms(PatientSymptoms),
+        not_symptoms(NotSymptoms),
+        count_unasked_symptoms(AllSymptoms, PatientSymptoms, NotSymptoms, CountUA),
+        CountUA > 0
+    ),
+    (
+        all_possible_symptoms(PossibleSymptoms),
+        common_symptoms(CommonSymptoms),
+        subtract(PossibleSymptoms, CommonSymptoms, SymptomsOI),
+        length(SymptomsOI, CountSOI),
+        CountSOI > 0
+    ).
+
 % loop for asking for symptoms
 possible_interview() :-
-    pi_not_over(),
     all_possible_symptoms(PossibleSymptoms),
     common_symptoms(CommonSymptoms),
     subtract(PossibleSymptoms, CommonSymptoms, SymptomsOI),
-    write(SymptomsOI),
-    nth0(0, SymptomsOI, SymptomOI),
+    random_member(SymptomOI, SymptomsOI),
     ask(SymptomOI),
-    symptoms_interview().
+    pi_not_over() -> possible_interview() ; true.
+
+
+trisks_interview([]).
+trisks_interview([H|T]) :-
+    scan_mins(H),
+    scan_trisks(H),
+    trisks_interview(T).
+
 
 % loop for asking for trisks (test/risks)
-trisks_interview() :-
-    possible(PossibleDisease),
-    scan(PossibleDisease),
-
-    trisks_interview().
 
 suggest(Diseases) :-
     length(Diseases, Length),
@@ -294,14 +373,25 @@ suggest(Diseases) :-
         Length == 0 ->
         write("We are not able to diagnose you with any diseases based on your symptoms and risk factors")
         ;
-        format("We are not able to diagnose you with any diseases based on your symptoms and risk factors, but we suggest that you get tested for ~s in an appropriate medical facility.", [Diseases])
+        format("We are not able to diagnose you with any diseases based on your symptoms and risk factors, but we suggest that you get tested for ~w in an appropriate medical facility.", [Diseases])
      ).
 
+diagnose(Disease) :-
+    format("We diagnose you with ~s", [Disease]).
+
 interview() :-
-    possible_interview(),
-    trisks_interview(),
-    possible_diseases(PossibleDiseases),
-    (
-        ( possible(Disease), hi_sus(Disease) ) ->
-        diagnose(Disease); suggest(PossibleDiseases)
-    ).
+     possible_interview(),
+     possible_diseases(PossibleDiseases),
+     (trisks_interview(PossibleDiseases) ; true),
+     possible_diseases(PossibleDiseases),
+     (
+         ( possible(Disease), hi_sus(Disease) ) ->
+         diagnose(Disease); suggest(PossibleDiseases)
+     ).
+
+
+
+undo :-
+    retractall(patient_symptoms(_)),
+    retractall(not_symptoms(_)),
+    retractall(patient_trisks(_)).
